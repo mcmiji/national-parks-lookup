400cd12a0ae7de661d7491e51c43075c32f2854a
mcmiji <mcmiji@gmail.com>
2016-09-20 23:32:17 -0700

    Create national_parks_by_state.py

---
+++ national_parks_by_state.py
@@ -0,0 +1,93 @@
+
+
+
+national_parks_by_state = {"alaska": ["Denali National Park and Preserve", 
+									"Gates of the Arctic National Park", 
+									"Glacier Bay National Park", 
+									"Katmai National Park and Preserve", 
+									"Kenai Fjords National Park",
+									"Kobuk Valley National Park",
+									"Lake Clark National Park"],
+							
+							"american samoa": ["National Park of American Samoa"],
+
+							"arizona":["Grand Canyon National Park", 
+									"Petrified Forest National Park", 
+									"Saguaro National Park"],
+
+							"arkansas": ["Hot Springs National Park"],
+
+							"california":["Channel Islands National Park", 
+										"Death Valley National Park", 
+										"Joshua Tree National Park", 
+										"Kings Canyon National Park", 
+										"Lassen Volcanic National Park",
+										"Pinnacles National Park",
+										"Redwood National Park",
+										"Sequoia National Park",
+										"Yosemite National Park"],
+
+
+							"colorado": ["Black Canyon of the Gunnison National Park", 
+									"Great Sand Dunes National Park and Preserve",
+									"Mesa Verde National Park",
+									"Rocky Mountain National Park"],
+
+							"florida": ["Biscayne National Park",
+									"Dry Tortugas National Park",
+									"Everglades National Park"],
+
+							"hawaii": ["Hawaii Volcanoes National Park", 
+									"Haleakala National Park"],
+
+							"kentucky": ["Mammoth Cave National Park"],
+
+							"maine": ["Acadia National Park"],
+
+							"michigan": ["Isle Royale National Park"],
+
+							"minnesota": ["[V]oyageurs National Park"],
+
+							"montana": ["Glacier National Park"],
+
+							"nevada": ["Great Basin National Park"],
+
+							"new mexico": ["Carlsbad Caverns National Park"],
+
+							"north carolina": ["Great Smoky Mountains National Park"],
+
+
+							"north dakota": ["Theodore Roosevelt National Park"],
+
+							"ohio": ["Cuyahoga Valley National Park"],
+
+							"oregon": ["Crater Lake National Park"],
+
+							"south carolina": ["Congaree National Park"],
+
+							"south dakota": ["Badlands National Park", 
+										"Wind Cave National Park"],
+
+							"tennessee": ["Great Smoky Mountains National Park"],
+	
+							"texas": ["Big Bend National Park",
+									"Guadalupe Mountains National Park"],
+
+							"virgin islands": ["Virgin Islands National Park"],
+
+							"utah": ["Arches National Park",
+								"Bryce Canyon National Park",
+								"Canyonlands National Park",
+								"Capitol Reef National Park",
+								"Zion National Park"],
+
+							"virginia": ["Shenandoah National Park"],
+
+							"washington": ["Mount Rainier National Park",
+									"North Cascades National Park",
+									"Olympic National Park"],
+
+							"wyoming": ["Grand Teton National Park",
+									"Yellowstone National Park"]
+							}
+
